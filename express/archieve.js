bdd58e3804ea0f7d3e20bd26c81eb1f5133da141
1106601034 <m13712901684@outlook.jp>
2025-07-27 20:27:34 +1000

    a

---
+++ express/archieve.js
@@ -0,0 +1,7 @@
+export function getFoo() {
+    return 'Foo';
+}
+
+export function add(a, b) {
+    return a + b;
+}
--- express/index.js
+++ express/index.js
@@ -1,12 +1,18 @@
-import express from 'express'; 
-// const express = require('express');
-const server = express();
-const port = 3000;
+// import express from 'express'; 
+// // const express = require('express');
+// const server = express();
+// const port = 3000;
 
-server.get('/hello', function (req, res) {
-    res.send('Hello World!');
-});
+// server.get('/hello', function (req, res) {
+//     res.send('Hello World!');
+// });
 
-server.listen(port, function () {
-    console.log('listening on ' + port);
-});
+// server.listen(port, function () {
+//     console.log('listening on ' + port);
+// });
+
+import { add, getFoo } from  './archieve.js';
+
+console.log(add(1,2));
+
+console.log(getFoo());
