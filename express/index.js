a3a6b4863739026de584dca202042983ac7d88a3
Christopher <christopherchan3003@gmail.com>
2025-07-30 15:45:37 +0800

    a

--- express/index.js
+++ express/index.js
@@ -1,40 +1,16 @@
-// Require the express web application framework (https://expressjs.com)
-import express from 'express';
+// import express from 'express'; 
+// // const express = require('express');
+// const server = express();
+// const port = 3000;
 
-// Create a new web application by calling the express function
-let app = express()
-// Get port from environment and store in Express.
-let port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+// server.get('/hello', function (req, res) {
+//     res.send('Hello World!');
+// });
 
-/**
- * Normalize a port into a number, string, or false.
- */
-function normalizePort(val) {
-  let port = parseInt(val, 10);
+// server.listen(port, function () {
+//     console.log('listening on ' + port);
+// });
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+import { Toolkits } from  './Toolkits.js';
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-
-// Tell our application to serve all the files under the `public_html` directory
-app.use(static('public_html'))
-
-// Tell our application to listen to requests at port 3000 on the localhost
-app.listen(port, function () {
-  // When the application starts, print to the console that our app is
-  // running at http://localhost:3000  (where the port number is 3000 by
-  // default). Print another message indicating how to shut the server down.
-  console.log(`Web server running at: http://localhost:${port}`)
-  console.log("Type Ctrl+C to shut down the web server")
-})
+Toolkits.quit('a');
