754c69f549940a20da9b9a8eef38c67e44daacb1
1106601034 <christopherchan3003@gmail.com>
2025-08-23 22:40:55 +0800

    a

--- nextapp/app/Archieve/Archieve.tsx
+++ nextapp/app/Archieve/Archieve.tsx
@@ -1,5 +1,5 @@
 // --------------------------
-// Stackt Symbol
+// Stack Symbol
 // --------------------------
 export function StacktSymbol(params: number) {
   for (let index = 1; index <= params; index++) {
@@ -57,3 +57,81 @@ export function tag(literal: any, ...values: any): string {
   return `${values[0]}${literal[1]}${values[1]} is ${result}`;
 }
 // --------------------------
+//  Shopping List
+// --------------------------
+export class ShoppingCart {
+  private items: Item[];
+
+  public constructor() {
+    this.items = [];
+  }
+
+  public getContents() {
+    return this.items;
+  }
+}
+
+export class Item {
+  private name: string;
+  private price: number;
+  private quantity: number;
+
+  public constructor(name: string, price: number, quantity: number) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  public getName() {
+    return this.name;
+  }
+
+  public getPrice() {
+    return this.price;
+  }
+
+  public getQuantity() {
+    return this.quantity;
+  }
+
+  public PrintItem() {
+    return `Name: ${this.name} Price: ${this.price} Quantity: ${this.quantity}`;
+  }
+}
+
+export function PrintItems(List: any[]): string {
+  if (List.length > 5) {
+    console.log("Your cart is full!");
+  }
+
+  let text: string = "";
+  for (let index = 1; index <= List.length; index++) {
+    text = `${text} ${index}. ${List[index - 1]}`;
+  }
+  return text;
+}
+
+export function AppendItems(List: any[], Items: any[]): any[] {
+  let newList: any[] = List.slice();
+  for (let index = 0; index < Items.length; index++) {
+    newList.push(Items[index]);
+  }
+  return newList;
+}
+
+export function RemoveItems(List: any[], ItemIndex: number[]): any[] {
+  let newList: any[] = List.slice();
+  for (let index = 0; index < ItemIndex.length; index++) {
+    let Item: number = ItemIndex[index];
+    newList.splice(Item, 1)
+  }
+  return newList;
+}
+
+export function SearchItems(List: any[], Items: string[]): boolean[] {
+  let CheckList: boolean[] = [];
+  for (let index = 0; index < Items.length; index++) {
+    CheckList.push(List.includes(Items[index]));
+  }
+  return CheckList;
+}
--- nextapp/app/page.tsx
+++ nextapp/app/page.tsx
@@ -2,10 +2,26 @@ import * as Archieve from "./Archieve/Archieve";
 import * as Toolkits from "./Archieve/Toolkits";
 
 export default function Home() {
-  let randomGoodiesToYOu: any = Toolkits.HereIsSomeGoodies();
+  const ShoppingList: string[] = ["Milk", "Egg", "Toast"];
+  const Keywords: string[] = ["Egg", "Apple", "Bunny"];
+  const ShoppingList2: string[] = Archieve.AppendItems(ShoppingList, ["Bunny", "Bunny"]);
+  const ShoppingList3: string[] = Archieve.RemoveItems(ShoppingList2, [-1]);
+  const ShoppingList4: boolean[] = Archieve.SearchItems(ShoppingList3, Keywords)
+  const item = new Archieve.Item("Milk", 12, 1);
+
   return (
     <div>
-      <h2>{randomGoodiesToYOu}</h2>
+      <h3>The Shopping List begins with:</h3>
+      <h4>{Archieve.PrintItems(ShoppingList)}.</h4>
+      <h3>Then we add two Bunnies and the list will be like:</h3>
+      <h4>{Archieve.PrintItems(ShoppingList2)}.</h4>
+      <h3>Then we remove the last Bunny and the list will be like:</h3>
+      <h4>{Archieve.PrintItems(ShoppingList3)}.</h4>
+      <h3>Then we check if follow items are in the list:</h3>
+      <h4>{Archieve.PrintItems(Keywords)}</h4>
+      <h4>{Archieve.PrintItems(ShoppingList4)}.</h4>
+      <h3>Then we create an object for MIlk the Item:</h3>
+      <h4>{item.PrintItem()}</h4>
     </div>
   );
 }
