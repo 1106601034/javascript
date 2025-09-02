003edb42f6acb7ee78e18b361ab47d9cfc90a562
1106601034 <christopherchan3003@gmail.com>
2025-09-02 16:27:56 +0800

    a

--- nextapp/app/src/app.tsx
+++ nextapp/app/src/app.tsx
@@ -1,22 +1,26 @@
 import * as Archieve from "./Archieve";
 import * as Toolkits from "./Toolkits";
 
-const theArray: number[] = [1, 2, 3, 4, 5];
+// const theArray: number[] = [1, 2, 3, 4, 5];
 
-function timer(item: number, callback: any) {
-    setTimeout(() => {
-        callback(item);
-    }, 2000);
-}
+// function timer(item: number, callback: any) {
+//     setTimeout(() => {
+//         callback(item);
+//     }, 2000);
+// }
 
-function processNumber(item: number) {
-    item = item * 2;
-    console.log(item);
-}
+// function processNumber(item: number) {
+//     item = item * 2;
+//     console.log(item);
+// }
+
+// function run(theArray: number[]) {
+//     for (let index = 0; index < theArray.length; index++) {
+//         const element = theArray[index];
+//         timer(element, processNumber);
+//     }
+// }
 
 export default function App() {
-    for (let index = 0; index < theArray.length; index++) {
-        const element = theArray[index];
-        timer(element, processNumber);
-    }
+
 }
