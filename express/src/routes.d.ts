4e9385f02aeabc1f77244ac70002cb72e99bf3d8
1106601034 <christopherchan3003@gmail.com>
2025-10-22 11:45:14 +1000

    move project

--- express/src/routes.d.ts
+++
@@ -1,5 +0,0 @@
-declare const routeHello: () => string;
-declare const routeAPINames: () => Promise<string>;
-declare const routeWeather: (query: WeatherQueryInterface) => WeatherDetailType;
-export { routeHello, routeAPINames, routeWeather, };
-//# sourceMappingURL=routes.d.ts.map
