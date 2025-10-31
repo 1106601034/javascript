4c5efcd9590f5c82b4492e46de0beaf9e4040952
1106601034 <christopherchan3003@gmail.com>
2025-10-31 23:45:48 +1000

    update

--- viteapp/src/components/ReduxExample/reduxPage.tsx
+++ viteapp/src/components/ReduxExample/reduxPage.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import Counter from "./Counter/counter";
 import Task from "./ToDoApp/task";
 import TaskList from "./ToDoApp/taskList";
 
 const ReduxPage = () => {
   return (
-    <div className="new-redux-project">
+    <div>
       <section>
         <h2>Counter</h2>
         <Counter />
