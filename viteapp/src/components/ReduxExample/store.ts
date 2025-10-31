2fc5e8e9925a3783b068f96165abf9e4ab87a0b5
1106601034 <christopherchan3003@gmail.com>
2025-10-31 23:42:24 +1000

    update

--- viteapp/src/components/ReduxExample/store.ts
+++ viteapp/src/components/ReduxExample/store.ts
@@ -1,15 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./Counter/rootReducer";
 import todoReducer from "./ToDoApp/rootReducer";
 
-const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    todo: todoReducer,
-  },
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  todo: todoReducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
