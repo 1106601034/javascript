99b59033cd884424a163487bea979b4cbbc22a46
1106601034 <christopherchan3003@gmail.com>
2025-11-04 17:41:10 +1000

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dataValidation from "./components/dataValidation/dataValidation.js";
 import greeting from "./components/helloWorld/helloWorld.js";
+import pageNotFound from "./components/pageNotFound/pageNotFound.js";
 
 const app = express();
 
@@ -8,11 +9,14 @@ app.use(express.json());
 
 app.get("/", greeting);
 
+app.get("*", pageNotFound);
+
 app.post(
     "/register",
     dataValidation.requirements,
     dataValidation.validation,
     dataValidation.dataValidation,
 );
+
 
 export default app;
---
+++ backend/src/components/pageNotFound/pageNotFound.js
@@ -0,0 +1,5 @@
+const PageNotFound = (req, res) => {
+    res.send("404 Page Not Found.");
+};
+
+export default PageNotFound;
