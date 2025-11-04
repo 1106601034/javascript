ab7ef5ed624ad12008b2cc1045c3ce5b8749b074
1106601034 <christopherchan3003@gmail.com>
2025-11-04 11:22:49 +1000

    update

--- backend/components/dataValidation/requirementMiddleware.js
+++ backend/components/dataValidation/requirementMiddleware.js
@@ -2,7 +2,7 @@ import { body } from "express-validator";
 
 const requirements = [
     body("name")
-        .notEmpty().withMessage("Name is required")
+        .notEmpty({ ignore_whitespace: true }).withMessage("Name is required")
         .trim()
         .isLength({ min: 2, max: 50 }).withMessage("Name must be between 2 and 50 characters"),
 
