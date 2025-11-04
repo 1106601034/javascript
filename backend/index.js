f14196953a36c758eef1104ba52d76d6305484f5
1106601034 <christopherchan3003@gmail.com>
2025-11-04 18:52:13 +1000

    update

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import config from "./config/config.js";
 import express from "express";
 import router from "./routes/routes.js";
+import pageNotFound from "./controllers/pageNotFound/pageNotFound.js";
 
 const app = express();
 
 app.use('/api', router);
-
 app.use(express.json());
+
+app.use(pageNotFound);
 
 app.listen(config.port, () => {
     console.log(`Server running on http://localhost:${config.port}`);
