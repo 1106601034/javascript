aa658fa5da5fef17e5837bc78db221acf3a498c4
1106601034 <christopherchan3003@gmail.com>
2025-11-04 17:51:49 +1000

    update

--- backend/src/components/pageNotFound/pageNotFound.js
+++ backend/src/components/pageNotFound/pageNotFound.js
@@ -1,5 +1,6 @@
 const PageNotFound = (req, res) => {
     res.send("404 Page Not Found.");
+    res.status(404);
 };
 
 export default PageNotFound;
---
+++ backend/src/controllers/example/userController.js
@@ -0,0 +1,19 @@
+const getAllUsers = (req, res) => {
+    const users = [
+        { id: 1, name: "Alice" },
+        { id: 2, name: "Bob" },
+        { id: 3, name: "Caroline" },
+    ];
+    res.json(users);
+};
+
+const getUserByID = (req, res) => {
+    const userID = req.params.id;
+    const user = {
+        id: userID,
+        name: 'Alice',
+    };
+    res.join(user);
+};
+
+export { getAllUsers, getUserByID };
