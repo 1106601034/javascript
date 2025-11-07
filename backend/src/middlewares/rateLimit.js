c6e8f45a998bb9bd2cf1d3e882c4285b0f55ae39
1106601034 <christopherchan3003@gmail.com>
2025-11-07 17:51:50 +1000

    update

---
+++ backend/src/middlewares/rateLimit.js
@@ -0,0 +1,8 @@
+const { rateLimit } = require('express-rate-limit');
+
+module.exports = rateLimit({
+  windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS) || 15 * 60 * 1000, // 15 minutes
+  limit: parseInt(process.env.RATE_LIMIT_REQUESTS_PER_WINDOW) || 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  skip: () => process.env.NODE_ENV === 'development',
+});
